04d36728630f91c5a48a40d804f9497278db263f
Akhil31261 <180031261@kluniversity.in>
2021-01-19 14:25:19 +0530

    adding files

---
+++ python.py
@@ -0,0 +1,7 @@
+print(""[DI Love DevOPs")
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Bitbucket[C[C[C[[C[C[C[C[C[C
+Compiling...
+
+
+Executed...
+Output: BitBucket is Great[A[A[A[A[A[D[D[D is great")
